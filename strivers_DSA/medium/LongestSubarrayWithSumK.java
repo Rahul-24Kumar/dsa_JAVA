264b992349b25a76bd2cfe225f919d5c4d15fed1
Rahul-24Kumar <24rahul.pcm@gmail.com>
2024-07-10 17:45:43 +0530

    LongestSubArrSum

---
+++ strivers_DSA/medium/LongestSubarrayWithSumK.java
@@ -0,0 +1,36 @@
+
+class LongestSubarrayWithSumK {
+
+    public static int longestSubArrSum (int[] arr, int k) {
+
+        int maxLength = 0;
+        int currentSum = 0;
+        int start = 0;
+
+
+        for (int end =0; end<arr.length; end++) {
+
+            currentSum += arr[end];
+
+            while(currentSum > k && start <= end) {
+                currentSum -= arr[start];
+                start++;
+            }
+
+            if(currentSum == k) {
+                maxLength = Math.max(maxLength, end - start + 1);
+            }
+        }
+
+        return maxLength;
+        
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 7, 5};
+        int K = 12;
+        int maxLength = longestSubArrSum(arr, K);
+        System.out.println("The length of the longest subarray with sum " + K + " is: " + maxLength);
+    }
+
+}
