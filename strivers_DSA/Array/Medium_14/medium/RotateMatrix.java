293d32c17b5bfad0b013dbcbfdd6d0b0b5663ab2
Rahul-24Kumar <24rahul.pcm@gmail.com>
2024-07-16 23:57:28 +0530

    RotateMatrix

---
+++ strivers_DSA/Array/Medium_14/medium/RotateMatrix.java
@@ -0,0 +1,44 @@
+public class RotateMatrix {
+    public static void rotate90Clockwise(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Swap columns
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        rotate90Clockwise(matrix);
+        printMatrix(matrix);
+    }
+}
