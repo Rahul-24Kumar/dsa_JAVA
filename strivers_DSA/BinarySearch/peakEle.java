b7dbe62a6055db18b08e788d733e818b5f6d1c8f
Rahul-24Kumar <24rahul.pcm@gmail.com>
2024-07-26 14:42:18 +0530

    peakElement

---
+++ strivers_DSA/BinarySearch/peakEle.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class peakEle {
+
+    public static int findPeakElements (ArrayList<Integer> arr) {
+        int n = arr.size();
+
+        if (n == 1) return 0;
+        if(arr.get(0) > arr.get(1)) return 0;
+        if(arr.get(n-1) > arr.get(n-2)) return n-1;
+
+        int low = 1, high = n -2;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if(arr.get(mid) > arr.get(mid - 1) && arr.get(mid) > arr.get(mid+1) )
+                return mid;
+            
+            if(arr.get(mid) > arr.get(mid-1)) low = mid + 1;
+
+            else high = mid - 1;
+           
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr =
+            new ArrayList<>(Arrays.asList(1, 2, 3, 8, 5, 6, 7, 8, 5, 1));
+        int ans = findPeakElements(arr);
+        System.out.println("The peak is at index: " + ans);
+    }
+}
