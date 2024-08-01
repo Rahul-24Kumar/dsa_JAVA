dfd5dac670b4797def6aefe67924c27a0f0945d6
Rahul-24Kumar <24rahul.pcm@gmail.com>
2024-08-01 23:26:46 +0530

    countRotationUsingBST

--- strivers_DSA/BinarySearch/RotationCount.java
+++ strivers_DSA/BinarySearch/RotationCount.java
@@ -1,23 +1,27 @@
 
 public class RotationCount {
     public static int RotationCount (int[] arr) {
-        int count = 0;
-
-       
+        int left = 0;
+        int right = arr.length -1;
+   
 
         while (left < right) {
-            int left = 0;
-            int right = arr.length -1;
-
+            
             int mid = left + (right - left)/2;
 
-            if ()
+            if (arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
         }
+
+        return left;
     }
 
 
     public static void main (String[] args) {
-        int[] arr = {4, 5, 6, 1, 2, 3};
+        int[] arr = {4, 5, 6, 7, 2, 3};
 
         int countRotation = RotationCount(arr);
         System.out.println(countRotation);
