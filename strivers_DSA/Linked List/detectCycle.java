0c9156e6335564ed981728693c21ecb40a09fb3b
rahul-24kumar <24rahul.pcm@gmail.com>
2024-09-26 22:55:14 +0530

    detectCycle

---
+++ strivers_DSA/Linked List/detectCycle.java
@@ -0,0 +1,56 @@
+
+class Node {
+    public int data;
+    public Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class detectCycle {
+
+    public static boolean detectCycle(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        // create a loop
+        fifth.next = third;
+
+        if (detectCycle(head)) {
+            System.out.println("Cycle detected");
+        } else {
+            System.out.println("No cycle detected");
+        }
+    }
+
+}
