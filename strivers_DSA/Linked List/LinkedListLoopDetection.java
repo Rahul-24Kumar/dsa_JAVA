59d5c4d996d16fc2c92634f3373d3a898264c536
rahul-24kumar <24rahul.pcm@gmail.com>
2024-09-27 23:46:44 +0530

    LinkedListLoopDetection

---
+++ strivers_DSA/Linked List/LinkedListLoopDetection.java
@@ -0,0 +1,61 @@
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1, Node next1) {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1) {
+        data = data1;
+        next = null;
+    }
+}
+
+public class LinkedListLoopDetection {
+
+    public static Node firstNode(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        node1.next = node2;
+        Node node3 = new Node(3);
+        node2.next = node3;
+        Node node4 = new Node(4);
+        node3.next = node4;
+        Node node5 = new Node(5);
+        node4.next = node5;
+
+        node5.next = node2;
+        Node head = node1;
+
+        Node loopStartNode = firstNode(head);
+
+        if (loopStartNode != null) {
+            System.out.println("Loop start node: " + loopStartNode.data);
+        } else {
+            System.out.println("No loop detected");
+        }
+    }
+}
